245613a580ce5ce19112a0bcc9084edb6d2bdc17
Messyas <messyasgoisfranca@gmail.com>
2024-01-01 21:48:59 -0400

    classe carro3

--- .vscode/primeiroPrograma/classes/classe3.cpp
+++ .vscode/primeiroPrograma/classes/classe3.cpp
@@ -2,9 +2,76 @@
 
 using namespace std;
 
+class carro{
+    private:
+
+    int ano;
+    float valor;
+    float km;
+
+    public:
+     //constructor
+     carro(int ano, float valor, float km);
+
+     void setano(int a);
+
+     int getano();
+
+     void setValor(float valor);
+
+     float getValor();
+
+     void setKm(float km);
+
+     float getKm();
+};
 
 int main() {
+
+    carro fusca(1989,30.000,90.0000);
+
+    cout << "Fusca: \n";
+    cout << "Ano: " << fusca.getano() << endl;    
+    cout << "Valor: " << fusca.getValor() << endl;    
+    cout << "Kms: " << fusca.getKm() << endl; 
 
     
     return 0;
 }
+
+//colocando o nome da classe e :: antes no nome da funcao 
+//é possivel sobrescrever os metodos da classe carro
+   carro::carro(int ano = 0, float valor = -1, float km = -1) {
+         this->ano = ano;
+         this->valor = valor;
+         this->km = km;
+    }
+
+    //ano
+     void carro::setano(int a) {
+       this->ano = ano;
+     }
+
+     int carro::getano() {
+        return ano;
+     }
+
+     //valor
+     void carro::setValor(float valor){
+        this->valor = valor;
+     }
+
+     float carro::getValor() {
+        return valor;
+     }
+
+     //KM
+
+     void carro::setKm(float km) {
+        this-> km = km;
+     }
+
+     float carro::getKm() {
+        return km;
+     }
+
